8d092912b35ea4fa6259c84f7e7f6b1cdcb06eb1
Jon Carr <4412666+jwcarr@users.noreply.github.com>
2022-03-03 16:49:08 +0100

    Create data directory automatically if it doesn't exist

--- class6/transform.py
+++ class6/transform.py
@@ -4,6 +4,10 @@ import csv
 
 DATA_DIR = Path('data')
 
+# If the data directory does not yet exist, create it
+if not DATA_DIR.exists():
+    DATA_DIR.mkdir()
+
 # Iterate over all the JSON files in the data directory...
 for file_name in DATA_DIR.iterdir():
     if file_name.suffix == '.json':
